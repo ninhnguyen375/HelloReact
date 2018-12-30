23b6ced11bf7a576d8dbd3ec62c8830c556b9db0
ninhnguyen375 <ninhnguyen375@gmail.com>
2018-12-30 09:44:45 +0700

    hello world

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import logo from './logo.svg';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello World</h1>
+        <img src={logo} height="300px" alt="something"/>
+        {/* <Button>Click To Say Hello</Button> */}
       </div>
     );
   }
